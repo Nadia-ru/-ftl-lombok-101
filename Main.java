45e1746a30b562ee5536042c70c54fd2415189f5
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-05 19:58:29 +0300

    task12

--- Main.java
+++ Main.java
@@ -1,13 +1,16 @@
-import task11.ValueExample;
+import task12.BuilderExample;
+
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args){
-        String[] tags = new String[]{"ddd"};
-        var x = new ValueExample("Nadya",21,5.0f,tags);
-        System.out.println(x.getAge());
-        System.out.println(x);
-        String s ="ddd";
-        var x2 = new Exception("tt");
-        System.out.println(x2);
+        BuilderExample example = BuilderExample.builder()
+                .created(12)
+                .age(21)
+                .name("Pasha")
+                .occupation("programing")
+                .occupations(Collections.singleton("cooking"))
+                .build();
+        System.out.println(example);
     }
 }
