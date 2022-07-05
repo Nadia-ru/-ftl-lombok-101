f55a926c929824012e9949a8f84705d2fdcec42f
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-05 20:56:06 +0300

    task14

---
+++ task14/WithExample.java
@@ -0,0 +1,16 @@
+package task14;
+import lombok.AccessLevel;
+import lombok.NonNull;
+import lombok.ToString;
+import lombok.With;
+@ToString
+public class WithExample {
+    @With(AccessLevel.PROTECTED) @NonNull private final String name;
+    @With private final int age;
+
+    public WithExample(String name, int age) {
+        if (name == null) throw new NullPointerException();
+        this.name = name;
+        this.age = age;
+    }
+}
