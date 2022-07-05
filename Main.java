9b1fc468e4ea7b8c1e3c6b173fa7b82e4779388d
nadia.trfm <69869414+Nadia-ru@users.noreply.github.com>
2022-07-05 15:25:27 +0300

    task2

---
+++ Main.java
@@ -0,0 +1,10 @@
+import task2.ValExample;
+
+public class Main {
+    public static void main(String[] args){
+        ValExample x1 = new ValExample();
+        String x = x1.example();
+        System.out.println(x);
+        x1.example2();
+    }
+}
